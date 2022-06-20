c0206060d301de3a43a8fa84bb01e61ebb9c9c10
Jonathan <jonathan.osagie@accenture.com>
2022-06-20 18:23:51 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function openNav(){
+    document.getElementById("items").style.width = "250px";
+    document.getElementById("sidebar-close").style.marginRight = "100px";
+
+}
+
+function closeNav(){
+    document.getElementById("items").style.width = "0px";
+    document.getElementById("sidebar-close").style.marginRight = "0px";
+
+}
