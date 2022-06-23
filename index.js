62a61b5756f1876567fe2dca91be3b67503cf958
Jonathan <jonathan.osagie@accenture.com>
2022-06-23 02:24:37 +0200

    proper centering; sidebar update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function openNav(){
-    document.getElementById("items").style.width = "25%";
-    document.getElementById("sidebar-close").classList.remove("header-sidebar__btn--invis");
-    document.getElementById("sidebar-close").style.marginRight = "25%";
+    document.getElementById("items").style.width = "100%";
+    document.getElementById("sidebar-close").classList.remove("header-sidebar--invis");
+    // document.getElementById("sidebar-close").style.marginRight = "25%";
 }
 
 function closeNav(){
-    document.getElementById("items").style.width = "0px";
+    document.getElementById("items").style.width = "0%";
     document.getElementById("sidebar-close").style.marginRight = "0px";
 
 }
